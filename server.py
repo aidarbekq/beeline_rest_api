b5bf4e5305d1453b3ed5041eb6932a7c715b3367
aidarbek <aidarbekabdyrakmanov@gmail.com>
2022-12-01 11:10:50 +0600

    remove async

--- server.py
+++ server.py
@@ -9,15 +9,15 @@ PORT = 8000
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
-    async def run_name(self, name):
+    def run_name(self, name):
         print(f'running {name}...')
 
-    async def send_my_headers(self):
+    def send_my_headers(self):
         self.send_response(200)
         self.send_header('content-type', 'application/json')
         self.end_headers()
 
-    async def only_json_response(self, content_type):
+    def only_json_response(self, content_type):
         self.send_response(400)
         self.send_header('content-type', content_type)
         self.end_headers()
@@ -26,21 +26,20 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(response.getvalue())
         return
 
-    async def received_response(self, data):  # эта функция возвращает ответ на POST метод
-        await self.send_my_headers()
+    def received_response(self, data):  # эта функция возвращает ответ на POST метод
+        self.send_my_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
     async def do_POST(self):
         content_type, pdict = cgi.parse_header(self.headers.get('content-type'))  # получаем тип запроса
         if content_type != 'application/json':  # проверка типа на JSON
-            await self.only_json_response(content_type)
+            self.only_json_response(content_type)
 
         # если проходит проверку, выполняется следующий код:
         else:
             length = int(self.headers.get('content-length'))  # получаем размер запроса
             post_data = json.loads(self.rfile.read(length))   # содержимое запроса
-            await self.received_response(post_data)
-
+            self.received_response(post_data)
 
 
 my_handler = Handler
