dcfbc59e3d5d834a0b3908165eff9aa6bc3392ee
aidarbek <aidarbekabdyrakmanov@gmail.com>
2022-12-02 09:30:34 +0600

    modify code
    add function printing_name

--- server.py
+++ server.py
@@ -1,23 +1,37 @@
 import http.server
 import json
-import socketserver
+import os
 from io import BytesIO
 import cgi
+from datetime import datetime
+import re
+from threading import Thread
+import pause
 
 PORT = 8000
 
 
+def printing_name():
+    """Функция отложенного исполнения"""
+    with open(f"post_json_data_.json", "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        json_time = data['time']  # получаем данные "time" отправленных клиентами
+        times = re.split('-| |:', json_time)   # преобразовываем данные 'time' в datetime(python) формат
+        schedule_time = datetime(int(times[0]), int(times[1]), int(times[2]), int(times[3]), int(times[4]))
+        pause.until(schedule_time)
+    print(data['name']) # вывод "name" в консоль
+
+
 class Handler(http.server.SimpleHTTPRequestHandler):
 
-    def run_name(self, name):
-        print(f'running {name}...')
-
     def send_my_headers(self):
+        """Функция для отправки хэдеров"""
         self.send_response(200)
         self.send_header('content-type', 'application/json')
         self.end_headers()
 
     def only_json_response(self, content_type):
+        """Если клиент отправляет не JSON, выполняется эта функция"""
         self.send_response(400)
         self.send_header('content-type', content_type)
         self.end_headers()
@@ -26,11 +40,13 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(response.getvalue())
         return
 
-    def received_response(self, data):  # эта функция возвращает ответ на POST метод
+    def received_response(self, data):
+        """эта функция возвращает ответ на POST метод"""
         self.send_my_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
-    async def do_POST(self):
+    def do_POST(self):
+        """POST метод"""
         content_type, pdict = cgi.parse_header(self.headers.get('content-type'))  # получаем тип запроса
         if content_type != 'application/json':  # проверка типа на JSON
             self.only_json_response(content_type)
@@ -38,18 +54,28 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         # если проходит проверку, выполняется следующий код:
         else:
             length = int(self.headers.get('content-length'))  # получаем размер запроса
-            post_data = json.loads(self.rfile.read(length))   # содержимое запроса
-            self.received_response(post_data)
+            post_data = json.loads(self.rfile.read(length))  # содержимое запроса
+            if post_data:
+                self.received_response(post_data)
+                with open("sample.json", "w") as outfile:
+                    json.dump(post_data, outfile)
+                if os.path.exists(f"sample.json"):
+                    thread = Thread(target=printing_name, daemon=True)
+                    thread.start()
 
 
 my_handler = Handler
 
-try:
-    with socketserver.ThreadingTCPServer(("127.0.0.1", PORT), my_handler) as httpd:
-        print(f"Starting at http://127.0.0.1:{PORT}\nNow you can send POST request.\nUse POSTMAN or curl.")
-        httpd.serve_forever()
 
-except KeyboardInterrupt:
+try:
+    with http.server.ThreadingHTTPServer(("127.0.0.1", PORT), my_handler) as httpd:
+        print(f"Starting at http://127.0.0.1:{PORT}\nNow you can send POST request.\nCTRL+C to STOP.")
+        httpd.serve_forever()   # запуск сервера
+
+
+except KeyboardInterrupt:  # CTRL+C
     httpd.shutdown()
     print("Stopping server")
     httpd.server_close()
+
+
